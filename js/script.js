272f183d65a42eb562f20cdc90a35f515f37e14f
nezzabunar <rahman.cop@gmail.com>
2023-06-22 22:14:12 +0700

    fix-convert-function

--- js/script.js
+++ js/script.js
@@ -1,44 +1,74 @@
+var nilai = document.getElementById("nilai");
+var hasil = document.getElementById('hasil');
+var label = document.getElementById('label');
+var labelCelcius = document.getElementById("labelCelcius");
+var labelFahrenheit = document.getElementById("labelFahrenheit");
+var notif = document.getElementById("notif");
+var button = document.getElementById('konversiCelcius');
 
-function konversi() {
-    let nilai = document.getElementById("nilai").value;
-    let hasil = document.getElementById('hasil');
-    let fahrenheit = document.getElementById('fahrenheit');
-
+function konversiKeCelcius() {
     // validasi jika input kosong
-    if (nilai == '') {
-        document.getElementById("notif").innerHTML = 'Nilai Tidak Boleh Kosong';
+
+    if (nilai.value == '') {
+        notif.innerHTML = 'Nilai Tidak Boleh Kosong';
     } else {
-        document.getElementById("notif").innerHTML = '';
-        document.getElementById('fahrenheit').value = '';
+        notif.innerHTML = '';
+        label.value = '';
 
         // perhitungan untukk konversi celcisu ke fahrenheit
-        result = nilai * 9 / 5 + 32;
 
-        fahrenheit.value = result;
-        hasil.value = `Nilai ${nilai}°C = ${result.toFixed(2)}°F`;
+        result = nilai.value * 9 / 5 + 32;
+        label.value = result.toFixed(2);
+        hasil.value = `Nilai ${nilai.value}°C = ${result.toFixed(2)}°F`;
+    }
+}
+
+function konversiKeFahrenheit() {
+    // validasi jika input kosong
+
+    if (nilai.value == '') {
+        notif.innerHTML = 'Nilai Tidak Boleh Kosong';
+    } else {
+        notif.innerHTML = '';
+        label.value = '';
+
+        // perhitungan untukk konversi celcisu ke fahrenheit
+
+        result = (nilai.value - 32) * 5 / 9;
+        label.value = result.toFixed(2);
+        hasil.value = `Nilai ${nilai.value}°F = ${result.toFixed(2)}°C`;
     }
 }
 
 function reset() {
-    document.getElementById("nilai").value = '';
-    document.getElementById("hasil").value = '';
-    document.getElementById("fahrenheit").value = '';
-    document.getElementById("notif").innerHTML = '';
+
+    // function untuk mereset ulang input dan label
+
+    nilai.value = '';
+    hasil.value = '';
+    label.value = '';
+    notif.innerHTML = '';
 }
 
 function reverse() {
-    let nilai = document.getElementById("nilai").value;
-    let hasil = document.getElementById('hasil');
+    // function reverse membalikan konversi dari celcis ke Fahrenheit atau sebaliknya
 
-    // validasi jika input kosong
-    if (nilai == '' || nilai == NaN) {
-        document.getElementById("notif").innerHTML = 'Nilai Tidak Boleh Kosong';
+    if (button.name === 'konversiCelcius') {
+
+        // cek apakah button name sekakrang adalah konver ke celcius 
+        // atau fahrenheit untuk mengubah pemangilan function pada tombol
+        // konversi
+
+        button.setAttribute("onclick", "konversiKeFahrenheit()");
+        labelCelcius.innerHTML = 'Fahrenheit °F';
+        labelFahrenheit.innerHTML = 'Celcius °C';
+        button.name = 'konversifahrenheit'
     } else {
-        document.getElementById("notif").innerHTML = '';
-        result = nilai - 32 / 1.8;
+        button.setAttribute("onclick", "konversiKeCelcius()");
+        labelCelcius.innerHTML = 'Celcius °C';
+        labelFahrenheit.innerHTML = 'Fahrenheit °F';
+        button.name = 'konversiCelcius'
 
-        // perhitungan untukk konversi fahrenheit ke celcius
-        // dengan result ditampikan padaa textarea
-        hasil.value = `Nilai ${nilai}°F = ${result.toFixed(2)}°C`;
     }
+    reset();
 }
